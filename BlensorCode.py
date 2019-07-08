3efda396756666662bffd712184b4fe910038124
JusticeZQ <42510829+JusticeZQ@users.noreply.github.com>
2019-07-08 14:37:02 +0800

    Update BlensorCode.py

--- BlensorCode.py
+++ BlensorCode.py
@@ -6,20 +6,23 @@ from mathutils import *
 from math import *
 import blensor
 
-    
-scanner = bpy.data.objects["Camera"]
+"""set the parameters of tof """
+scanner = bpy.data.objects["Camera"];
+scanner.location = (0,-10,2);
+scanner.rotation_euler= (90/180*pi,0,0);
+scanner.scan_type = "tof";
 
-"""Move it to a different location"""
-scanner.location = (0,-10,2)
-scanner.rotation_euler= (90/180*pi,0,0)
-scanner.scan_type = "tof"
-
-print("\n\n\n\n\n\n")
-
+"""clear all scanning datas  """
 for item in bpy.data.objects:
     if item.type == 'MESH' and item.name.startswith('Scan'):
         bpy.data.objects.remove(item)
-bpy.data.objects["Satellite"].rotation_euler=(60/180*pi,0,0)
+
+"""set the position and attitude of satellite"""
+satellite=bpy.data.objects["Satellite"];
+satellite.rotation_euler=(60/180*pi,0,0);
+satellite.location=(0,0,0);
+
+"""clear the scanning in view windows and start newly scan"""
 bpy.ops.blensor.delete_scans();
 bpy.ops.blensor.scan();
 
@@ -27,12 +30,11 @@ bpy.ops.blensor.scan();
 f=open("C:/Users/Justice/Desktop/111/test0.txt","w")
 for item in bpy.data.objects:
     if item.type == 'MESH' and item.name.startswith('Scan'):
-        # Scannerpunkte durchgehen
         for sp in item.data.vertices:
-            print('X=%+#5.3f\tY=%+#5.3f\tZ=%+#5.3f' % (sp.co[0], sp.co[1],sp.co[2]))
-            str='%#5.3f\t%#5.3f\t%#5.3f \n' % (sp.co[0], sp.co[1],sp.co[2])
-            f.write(str)
-        print("1 OK")
+            print('X=%+#5.3f\tY=%+#5.3f\tZ=%+#5.3f' % (sp.co[0], sp.co[1],sp.co[2]));
+            str='%#5.3f\t%#5.3f\t%#5.3f \n' % (sp.co[0], sp.co[1],sp.co[2]);
+            f.write(str);
+        print("1 OK");
            
            
            
